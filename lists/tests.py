c0150594af41be07dfcebe71818e95bc49a32030
MHWillard <mhwillard@gmail.com>
2023-02-28 15:52:41 -0500

    2.28.23 ch3

---
+++ lists/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_resolve(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
