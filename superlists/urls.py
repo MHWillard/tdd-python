ba17c0375d911f991c35aef0a199049a4ee68224
MHWillard <mhwillard@gmail.com>
2023-03-17 15:26:32 -0400

    made changes to get tests to pass on new Django

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/', include(list_urls)),
+    re_path(r'^$', list_views.home_page, name='home'),
+    re_path(r'^lists/', include(list_urls)),
 ]
 
